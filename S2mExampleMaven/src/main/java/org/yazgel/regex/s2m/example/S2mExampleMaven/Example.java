917bc9101fc5d2df431eaebd80136ba174173de6
akgun demirbas <akgun.demirbas@gmail.com>
2016-01-23 16:32:05 +0200

    Added maven sample.

--- S2mExampleMaven/src/main/java/org/yazgel/regex/s2m/example/S2mExampleMaven/Example.java
+++ S2mExampleMaven/src/main/java/org/yazgel/regex/s2m/example/S2mExampleMaven/Example.java
@@ -1,31 +1,30 @@
-package example;
-
-import java.io.File;
-import java.util.Map;
-import java.util.Map.Entry;
-import java.util.Set;
+package org.yazgel.regex.s2m.example.S2mExampleMaven;
 
 import org.yazgel.regex.s2m.generator.GenerationType;
 import org.yazgel.regex.s2m.generator.S2MRunner;
 
+import java.util.Map;
+import java.util.Map.Entry;
+import java.util.Set;
 
 public class Example {
-	
+
 	private static final String COMMAND = "012345Serhat";
-	
+
 	public static void main(String[] args) {
-		String filePath = "file:/" + new File("model/deneme.s2m").getAbsolutePath();
-		
+		String filePath = "file:/" + Example.class.getResource("/model/deneme.s2m").getFile().toString();
+
 		long startTime = System.currentTimeMillis();
-		Map<String, Object> result = (Map<String, Object>) S2MRunner.getInstance().run(filePath, COMMAND, GenerationType.HASH_MAP);
-		long endTime   = System.currentTimeMillis();
-		
+		Map<String, Object> result = (Map<String, Object>) S2MRunner.getInstance().run(filePath, COMMAND,
+				GenerationType.HASH_MAP);
+		long endTime = System.currentTimeMillis();
+
 		long totalTime = endTime - startTime;
 		System.out.println(totalTime);
-		
+
 		Set<Entry<String, Object>> entrySet = result.entrySet();
 		for (Entry<String, Object> entry : entrySet) {
 			System.out.println(entry.getKey() + " => " + entry.getValue());
-		} 
+		}
 	}
 }
